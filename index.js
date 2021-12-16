bd5b1002e392275bca9e5b62fd7372ecf250f883
Vens <v.kolev@alfatrex.com>
2021-12-16 09:06:02 +0100

    JS fix

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ axios.get('https://api.nasa.gov/planetary/apod?api_key=TmmRW1tHiv0Q5h2h1x69FiWCw
     console.log(resp.data.date);
 });
 
-axios.get('https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=TmmRW1tHiv0Q5h2h1x69FiWCwa4HZOWwu1IoGukz').then(respm => {
-    document.getElementById("results").innerHTML = respm.data;
-    console.log(respm.data);
-    // console.log(resp.data.title);
-    // console.log(resp.data.explanation);
-    // console.log(resp.data.copyright);
-    // console.log(resp.data.date);
-});
+// axios.get('https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=TmmRW1tHiv0Q5h2h1x69FiWCwa4HZOWwu1IoGukz').then(respm => {
+//     document.getElementById("results").innerHTML = respm.data;
+//     console.log(respm.data);
+//     // console.log(resp.data.title);
+//     // console.log(resp.data.explanation);
+//     // console.log(resp.data.copyright);
+//     // console.log(resp.data.date);
+// });
